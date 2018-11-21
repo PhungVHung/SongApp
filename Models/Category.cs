ec5284a8858b1bc9d7b5ecfa6a4d2acefe8b4b9f
unknown <hungpvd00634@fpt.edu.vn>
2018-11-22 02:31:07 +0700

    Xac

---
+++ Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace My_Song.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<SongCategory> SongCategories { get; set; }
+    }
+}
---
+++ Models/MyDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace My_Song.Models
+{
+    public class MyDbContext: DbContext
+    {
+        public DbSet<Songs> Song { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<SongCategory> SongCategories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MySong;Trusted_Connection=True;MultipleActiveResultSets=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SongCategory>()
+                .HasKey(sc => new { sc.SongId, sc.CategoryId });
+        }
+    }
+}
